5000db78f11c7b595e1b1d5bf2e67a8417ff87bc
mdaries <mallory.daries@gmail.com>
2022-07-06 21:36:55 -0300

    Use Async coverage API
    Sync coverage API has been deprecated, coverage check is now using the recommended Sigfox Async coverage API

--- check_coverage.py
+++ check_coverage.py
@@ -3,23 +3,13 @@ import csv
 import json
 import logging
 import argparse
+import time
 
 import requests
 from requests.auth import HTTPBasicAuth
-import aiohttp
-import asyncio
-import async_timeout
-import tqdm
-
 
 
 import osm_api
-
-# ex https://towardsdatascience.com/fast-and-async-in-python-accelerate-your-requests-using-asyncio-62dafca83c33
-# https://medium.com/radix-ai-blog/performant-http-with-aiohttp-in-python-3-756580e54eff
-# https://pawelmhm.github.io/asyncio/python/aiohttp/2016/04/22/asyncio-aiohttp.html
-# aiohttp files writing https://www.blog.pythonlibrary.org/2016/11/09/an-intro-to-aiohttp/
-#try method 3(client task pool) or 4 method  "each" https://medium.com/@cgarciae/making-an-infinite-number-of-requests-with-python-aiohttp-pypeln-3a552b97dc95 
 
 # TO DO
 #implement legacy link quality indicator to render on the map
@@ -27,10 +17,12 @@ import osm_api
 #improve read csv : pull error line + CONTINUE
 #add permissionError mgmt in write result when file is open
 #create result in a folder/ add location file as an input
-DEBUG = True 
+#function to automatically set number of retry + delay between retry depending on #of location
 
-#param to be added 
-request_type = "coverage"
+DEBUG = True
+
+#param to be added
+request_type = "coverage_async"
 
 #default params
 default_credname = "root"
@@ -39,10 +31,13 @@ default_dev_class = 0
 
 #default input and output filepath
 current_folder = os.path.dirname(__file__)
-default_input_csv = os.path.join(current_folder,"location_small.csv")
+default_input_csv = os.path.join(current_folder,"location.csv")
 cred_file = os.path.join(current_folder,"credentials")
 default_output_csv = os.path.join(current_folder,"coverage_result.csv")
 
+#default get response param
+max_retry = 60
+delay_retry = 30
 
 #Logger Settings
 logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(levelname)s - %(message)s')
@@ -50,6 +45,7 @@ logging.basicConfig(level=logging.INFO,format='%(asctime)s - %(levelname)s - %(m
 
 base_url={
     "coverage":"https://backend.sigfox.com/api/v2/coverages/global/predictions",
+    "coverage_async":"https://api.sigfox.com/v2/coverages/global/predictions/bulk/"
 }
 
 #lqi algo constants
@@ -87,7 +83,7 @@ def read_cred(filename,cred_name,request_type):
                 valid = test_cred(cred_name,request_type,cred)
                 if valid:
                     logging.info("Credentials OK")
-                    return cred       
+                    return cred
         else:
             logging.critical(f"File not found {filename}")
             raise SystemExit
@@ -96,24 +92,25 @@ def read_cred(filename,cred_name,request_type):
         raise SystemExit
     except ValueError as e:
         logging.critical(f"Invalid JSON in credentials : {e}")
-        raise SystemExit    
+        raise SystemExit
     except Exception as e:
         logging.critical("Unexpected error while opening credentials")
         logging.critical(f"System returned {e.__class__.__name__}, {e}")
         raise SystemExit
 
 
-#Test credentials with an HTTP request 
+#Test credentials with an HTTP request
+#update the test with post
 def test_cred(cred_name,request_type,cred):
     if cred["login"] and cred["password"]:
-        if request_type == "coverage":
+        if request_type == "coverage" or request_type == "coverage_async":
             #Sigfox HQ lat,lng for test
             parameters = {"lat": 43.543925, "lng": 1.511323, "radius": 200}
             try:
                 response = requests.get(
-                base_url[request_type], 
-                auth=HTTPBasicAuth(cred["login"],cred["password"]), 
-                params = parameters 
+                base_url["coverage"],
+                auth=HTTPBasicAuth(cred["login"],cred["password"]),
+                params = parameters
                 )
                 response.raise_for_status()
                 if response.ok:
@@ -138,7 +135,7 @@ def read_location_from_csv(filename):
         #try to open file
         try:
             with open(filename,"r") as csv_file:
-                logging.info("Input csv file opened")
+                logging.info(f"Input csv file {filename} opened")
                 dialect = csv.Sniffer().sniff(csv_file.read(),delimiters=';,')
                 csv_file.seek(0)
                 csv_reader = csv.reader(csv_file, dialect)
@@ -146,7 +143,7 @@ def read_location_from_csv(filename):
                 errors = 0
                 dict_pos = {}
                 for row in csv_reader:
-                    #try if the index exist 
+                    #try if the index exist
                     try:
                         #check if row is a coord
                         if is_coord(row[0],row[1],line_count):
@@ -189,12 +186,12 @@ def read_result_from_csv(filename):
             for row in csv_reader:
                 #dict_result = {"pos":[lat,lng],"covered":covered,"margins":margins,"lqi":lqi}
                 results.append({"pos":[row[1],row[2]],"covered":bool(row[3]),"margins":[int(row[4]),int(row[5]),int(row[6])],"lqi":int(row[7])})
-                line_count+=1   
+                line_count+=1
             #print(results)
     else:
         logging.error(f"File not found {filename}")
         raise SystemExit
-        
+
     return results
 
 #write coverage results to output csv file
@@ -207,7 +204,7 @@ def write_result_to_csv(filename,results = []):
         line_count = 0
         for result in results:
             csv_writer.writerow([
-                result["id"],
+                #result["id"],
                 result["pos"][0],
                 result["pos"][1],
                 result["covered"],
@@ -224,7 +221,7 @@ def write_result_to_csv(filename,results = []):
 def get_lqi(margins):
     # lqi 1->limit, 2->poor, 3->average, 4->good, 5->very good, 6->Excellent
     #global_ranking_list = [min_tresh,lower_boundary,upper_boundary,redundancy,lqi]
-    
+
     if sum(margins) == 0:
         return 0
 
@@ -244,67 +241,86 @@ def get_lqi(margins):
     return lqi
 
 def retry_code(code: int) -> bool:
-    
+
         return 500 <= code <= 599 or code in statuses
 
-#Asynchronous function - call coverage API to get coverage result for a given lat,lng
-async def fetch_coverage_async(url,session, lat, lng, i, radius=200,current_attempt=0):
-    params = {"lat": lat,"lng":lng}
+def build_body(dict_pos):
+    locations = []
+    for i in dict_pos:
+        locations.append({"lat": dict_pos[i][0], "lng": dict_pos[i][1]})
+
+    body_str = {"locations":locations}
+    body_json = json.dumps(body_str)
+
+    return body_json
+
+def create_coverage_bulk(body_json,cred,radius=200):
     max_retry = 3
     retry_wait = 1
-    current_attempt +=1
-    global http_error 
-    url = "https://backend.sigfox.com/api/v2/coverages/global/prediction"
-    async with session.get(url,params=params) as response:
-        result = await response.json()
-        #covered = response.get('locationCovered')
-        code = response.status
-        
-        if current_attempt < max_retry and retry_code(code):
-            await asyncio.sleep(retry_wait)
-            return await fetch_coverage_async(url,session, lat, lng, i, radius, current_attempt)
-        if code == 200:
-            margins = result.get('margins')
-            # apply offset on margins tab
-            margins_offset = [m-settings["offset"] if m > settings["offset"] else 0 for m in margins]
-            lqi = get_lqi(margins_offset)
-            if lqi:
-                covered = True
-            else:
-                covered = False
-            #Format {'pos': ['-34.921403', '-54.945659'], 'covered': True, 'margins': [18, 0, 0],'lqi':3}
-            dict_result = {"id":i,"pos":[lat,lng],"covered":covered,"margins":margins_offset,"lqi":lqi,"api_call":"OK"}
-        else:
-            dict_result = {"id":i,"pos":[lat,lng],"covered":False,"margins":[-1,-1,-1],"lqi":-1,"api_call":f"Fail:Http {code}"}
-            http_error+=1
-
-    return dict_result
-
-#Asynchronous function - Fetch all responses within one Client session, keep connection alive for all requests.
-async def fetch_coverage_async_all(dict_pos,cred,request_type):
-
-    tasks = []
-    url = base_url[request_type]
-    radius = 200
-    timeout = None
-
-    auth = aiohttp.BasicAuth(login=cred["login"], password=cred["password"], encoding='utf-8')
-    logging.info("Starting http requests")
-    #limit number of simultaneous TCP connection (default=100)
-    conn = aiohttp.TCPConnector(limit=30)
-    session_timeout = aiohttp.ClientTimeout(
-        total=timeout,
-        sock_connect=60, 
-        sock_read=60
+    global http_error
+    headers = {'Content-type': 'application/json'}
+    try:
+        response = requests.post(
+            base_url[request_type],
+            data=body_json,
+            auth=HTTPBasicAuth(cred["login"], cred["password"]),
+            headers=headers
         )
-    async with aiohttp.ClientSession(auth=auth,connector=conn, timeout=session_timeout) as session:  
-        for i in dict_pos:
-            lat = dict_pos[i][0]
-            lng = dict_pos[i][1]
-            task = fetch_coverage_async(url,session,lat,lng,i+1,radius)
-            tasks.append(task)
-        responses = [await f for f in tqdm.tqdm(asyncio.as_completed(tasks), total=len(tasks),desc="Progression")]
-        return responses
+        response.raise_for_status()
+        if response.ok:
+            job_id = response.json().get("jobId")
+            logging.info(f"Coverage job {job_id} created")
+            return job_id
+        else:
+            return -1
+    except requests.HTTPError as e:
+        logging.error(f"HTTP error {e}")
+        raise SystemExit
+
+def get_job_result(job_id,cred):
+
+    url = base_url[request_type] + str(job_id)
+
+    i = 0
+    while i < max_retry :
+        i+=1
+        logging.info(f"Try {i} ")
+        try:
+            response = requests.get(
+                url=url,
+                auth=HTTPBasicAuth(cred["login"], cred["password"])
+            )
+            response.raise_for_status()
+            if response.ok:
+                if response.json().get("jobDone"):
+                    return response.json()
+        except requests.HTTPError as e:
+            logging.error(f"HTTP error {e}")
+            raise SystemExit
+        time.sleep(delay_retry)
+
+def process_response(response):
+    results = response.get('results')
+    list_results = []
+    line_count = 0
+    for result in results:
+        margins = result.get('margins')
+        lat = result.get('lat')
+        lng = result.get('lng')
+        # apply offset on margins tab
+        margins_offset = [m - settings["offset"] if m > settings["offset"] else 0 for m in margins]
+        lqi = get_lqi(margins_offset)
+        if not lqi:
+            covered: bool = False
+        else:
+            covered = True
+
+        dict_result = {"pos": [lat, lng], "covered": covered, "margins": margins_offset, "lqi": lqi,"api_call": "OK (200)"}
+        list_results.append(dict_result)
+        line_count += 1
+
+    return list_results
+
 
 if __name__ == '__main__':
 
@@ -359,9 +375,12 @@ if __name__ == '__main__':
     # offset = device attenuation + environment attenuation
     settings["offset"] = dev_atten[settings["dev_class"]] + env_atten[settings["env_type"]]
     logging.info(f"Product class: U{settings['dev_class']}, Environment: {settings['env_type']} => Attenuation applied = {settings['offset']}dB")
-    
+
     #read pos from csv
     dict_pos = read_location_from_csv(settings["input_file"])
+
+    #build Json body
+    body_json = build_body(dict_pos)
 
     #HTTP retry statuses
     statuses = {x for x in range(100, 600)}
@@ -370,17 +389,21 @@ if __name__ == '__main__':
 
     http_error = 0
 
-    #Get coverage from API
-    responses = asyncio.run(fetch_coverage_async_all(dict_pos,settings['credentials'],request_type))
-    if http_error:
-        logging.error(f"{http_error} API call failed : check the result file for more details")
-    results_sorted = sorted(responses, key=lambda k: k['id']) 
+    job_id = create_coverage_bulk(body_json,settings['credentials'])
+
+    time_start = time.perf_counter()
+    response = get_job_result(job_id,settings['credentials'])
+    time_end = time.perf_counter()
+    logging.info(f"Request processed in  : {time_end-time_start} seconds")
+
+
+    tab_results=process_response(response)
 
     #Write results to csv
-    write_result_to_csv(settings["output_file"],results_sorted)
+    write_result_to_csv(settings["output_file"],tab_results)
 
     #read result from csv
     #responses = read_result_from_csv(input_res_csv)
 
     # Create map with results
-    osm_api.create_map(results_sorted,settings['clustering'])
+    osm_api.create_map(tab_results,settings['clustering'])
